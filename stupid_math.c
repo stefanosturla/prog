905b1926414cc78e427dff721a551b9a0f13b4e7
Stefano Sturla <sturla.stefano@gmail.com>
2024-02-23 18:12:06 +0100

    Revert "Added complex mean function"
    This reverts commit 88088ac82c4ee6a8266bb9acb4541c4a96a278f3.

--- stupid_math.c
+++ stupid_math.c
@@ -4,12 +4,3 @@ float mean(int a, int b)
 {
 	return (a + b) / 2;
 }
-
-complex_float_t complex_mean(complex_int_t a, complex_int_t b) {
-	complex_float_t res= {0,0};
-	
-	res.re = mean(a.re, b.re);
-	res.im = mean(a.im, b.im);
-	
-	return res;
-}
--- stupid_math.h
+++ stupid_math.h
@@ -1,17 +1,6 @@
 #ifndef _STUPIDMATHH_
 #define _STUPIDMATHH_
 
-typedef struct complex_int {
-	int re;
-	int im;
-} complex_int_t;
-
-typedef struct complex_float {
-	float re;
-	float im;
-} complex_float_t;
-
 float mean(int a, int b);
-complex_float_t complex_mean(complex_int_t a, complex_int_t b);
 
 #endif
